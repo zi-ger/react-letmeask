9cfe17a1b778916619473a4376533e0e22df6757
Gustavo Ziger <gustavoziger@gmail.com>
2022-03-04 14:57:56 -0300

    Adds NewRoom page, navigation and authentication
    Creates the NewRoom page and its styling.
    Adds pages routes and navigation.
    Adds Firebase authentication using Google Account.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
 import { Button } from '../components/Button';
 
+import { auth, firebase } from '../services/firebase'
+
 import leftImage from '../assets/images/illustration.svg';
 import logoImage from '../assets/images/logo.svg';
 import googleIconImage from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
+import { useNavigate } from 'react-router-dom';
 
 export function Home() {
+  const navigate = useNavigate();
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(() => navigate('/rooms/new'));
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -17,7 +28,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImage} alt="letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImage} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
@@ -25,7 +36,7 @@ export function Home() {
           <form>
             <input
               type="text"
-              placeholder="Digie o código da sala"
+              placeholder="Digite o código da sala"
             />
             <Button type="submit">Entrar na sala</Button>
           </form>
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import { Button } from '../components/Button';
+
+import leftImage from '../assets/images/illustration.svg';
+import logoImage from '../assets/images/logo.svg';
+
+import '../styles/auth.scss';
+import { Link } from 'react-router-dom';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={leftImage} alt="Ilustração" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImage} alt="letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
